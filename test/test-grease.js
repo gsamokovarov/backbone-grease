39548df74edbb21f8ed0ead41dc6d1dae9d40d9e
Genadi Samokovarov <gsamokovarov@gmail.com>
2013-02-15 01:04:13 +0200

    Add tests

--- test/test-grease.js
+++ test/test-grease.js
@@ -1,5 +1,25 @@
 $(document).ready(function() {
 
-  module('Backbone.Grease');
+  var col;
+
+  module('Backbone.Grease', _.extend(new Environment, {
+    setup: function() {
+      Environment.prototype.setup.apply(this, arguments);
+
+      col = new Backbone.Collection([
+         new Backbone.Model({id: 3, label: 'a'}),
+         new Backbone.Model({id: 2, label: 'b'}),
+         new Backbone.Model({id: 1, label: 'c'}),
+         new Backbone.Model({id: 0, label: 'd'})
+      ]);
+    }
+  }));
+
+  test("Underscore Grease methods", 1, function() {
+    deepEqual(col
+      .filter_(function(o) { return o.id % 2 === 0; })
+      .map(function(o) { return o.id * 2; }),
+    [4, 0]);
+  });
 
 });
